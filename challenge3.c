daa6bde4abfebc36c6640cb720854c093a0ab81b
achbab oussama <oussamaachbab177@gmail.com>
2024-09-09 16:54:03 +0200

    chllenge3

---
+++ challenge3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+
+
+float k;
+float yard;
+
+
+printf ("please enter the distance you wanna convert to yards in kelometrs :");
+scanf ("%f", &k);
+
+yard = k * 1093.61;
+
+
+printf ("in yards is : %.2f", yard);
+
+
+
+
+}
